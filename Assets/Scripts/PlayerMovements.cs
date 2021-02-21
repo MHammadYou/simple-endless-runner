4c12073e0c6550b3ed88a1978264ad134a39b0a6
MHammadYou <MHammadY2.0@gmail.com>
2021-02-21 23:04:52 +0500

    Added Basic Player Movements

---
+++ Assets/Scripts/PlayerMovements.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class PlayerMovements : MonoBehaviour
+{
+
+    public Rigidbody rb;
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey("w"))
+        {
+            rb.AddForce(0, 0, 500 * Time.deltaTime);
+        } 
+        if (Input.GetKey("s"))
+        {
+            rb.AddForce(0, 0, -500 * Time.deltaTime);
+        } 
+        if (Input.GetKey("a"))
+        {
+            rb.AddForce(-500 * Time.deltaTime, 0, 0);
+        } 
+        if (Input.GetKey("d"))
+        {
+            rb.AddForce(500 * Time.deltaTime, 0, 0);
+        }
+    }
+}

5897afd78da6ba3a6d4cc34077ac3fde41c9af3a
MHammadYou <MHammadY2.0@gmail.com>
2021-02-22 18:05:58 +0500

    Game restarts after collisions or if player falls off the ground

---
+++ Assets/Scripts/HandleCollisions.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class HandleCollisions : MonoBehaviour
+{
+
+    public Transform player;
+
+    private bool _gameHasEnded = false;
+    
+    void Update()
+    {
+        if (player.position.y < -1f)
+        {
+            EndGame();
+        }
+    }
+
+    void EndGame()
+    {
+        if (!_gameHasEnded)
+        {
+            _gameHasEnded = true;
+            Debug.Log("GameOver");
+            Invoke("Restart", 0.5f);
+        }
+    }
+
+    void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            EndGame();
+        }
+    }
+}
--- Assets/Scripts/PlayerMovements.cs
+++ Assets/Scripts/PlayerMovements.cs
@@ -8,14 +8,9 @@ public class PlayerMovements : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        if (Input.GetKey("w"))
-        {
-            rb.AddForce(0, 0, 500 * Time.deltaTime);
-        } 
-        if (Input.GetKey("s"))
-        {
-            rb.AddForce(0, 0, -500 * Time.deltaTime);
-        } 
+
+        rb.AddForce(0, 0, 1000 * Time.deltaTime);
+ 
         if (Input.GetKey("a"))
         {
             rb.AddForce(-500 * Time.deltaTime, 0, 0);
